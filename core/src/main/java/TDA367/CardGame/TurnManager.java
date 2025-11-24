44185d002034b2e4d9a4ae6c14e8971a78a26e83
Natalie Glavak <natalieglavak@Mac.lan>
2025-11-24 15:53:10 +0100

    Skapa Turnbased-klass och implementera GameStrategy

---
+++ core/src/main/java/TDA367/CardGame/TurnManager.java
@@ -0,0 +1,22 @@
+package TDA367.CardGame;
+
+public class TurnManager {
+    private int current_p_index = 0;
+    private final int playerCount;
+
+    public TurnManager(int playerCount){
+        if (playerCount < 2){
+            throw new IllegalArgumentException("TurnManger requires at least 2 players");
+        }
+        this.playerCount = playerCount;
+    }
+
+    public int GetCurrentIndex(){
+        return current_p_index;
+    }
+
+    public void next(){
+        current_p_index = (current_p_index + 1) % playerCount;
+    }
+
+}
