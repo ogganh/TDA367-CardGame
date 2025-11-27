88f8669c39c46320d277d12a4083eaf28957f01d
ASmaller <linus@ordal.se>
2025-11-27 13:46:31 +0100

    Refactor Main class for improved readability and consistency

--- core/src/main/java/TDA367/CardGame/Main.java
+++ core/src/main/java/TDA367/CardGame/Main.java
@@ -12,7 +12,6 @@ import TDA367.CardGame.model.card_logic.Card;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
@@ -23,9 +22,11 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
+ * platforms.
+ */
 public class Main extends ApplicationAdapter {
     private SpriteBatch spriteBatch;
     FitViewport viewport;
@@ -38,6 +39,8 @@ public class Main extends ApplicationAdapter {
     GoFishUserPlayer player1;
     GoFishUserPlayer player2;
 
+    Boolean mouseIsDown = false;
+
     @Override
     public void create() {
         spriteBatch = new SpriteBatch();
@@ -46,7 +49,7 @@ public class Main extends ApplicationAdapter {
 
         font.getData().setScale(0.5f);
         viewport = new FitViewport(1980 / 4, 1080 / 4);
-        mainView = new MainView(font,viewport);
+        mainView = new MainView(font, viewport);
 
         player1 = new GoFishUserPlayer("Player 1");
         player2 = new GoFishUserPlayer("Player 2");
@@ -65,38 +68,39 @@ public class Main extends ApplicationAdapter {
     }
 
     private void input() {
-        if (Gdx.input.isKeyPressed(Input.Keys.ENTER)){
+        if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
             mainView.GoFish();
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.NUM_1)){
+        if (Gdx.input.isKeyPressed(Input.Keys.NUM_1)) {
             gameContext.handleTurn(new PlayerAction(gameContext.getCurrentPlayerIndex(), "ask", "ACE", "HEARTS"));
-            Gdx.app.log("Action", "Player " + Integer.toString(gameContext.getCurrentPlayerIndex()+1) + " asked for ACEs");
+            Gdx.app.log("Action",
+                    "Player " + Integer.toString(gameContext.getCurrentPlayerIndex() + 1) + " asked for ACEs");
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.NUM_2)){
+        if (Gdx.input.isKeyPressed(Input.Keys.NUM_2)) {
             gameContext.handleTurn(new PlayerAction(gameContext.getCurrentPlayerIndex(), "ask", "TWO", "HEARTS"));
-            Gdx.app.log("Action", "Player " + Integer.toString(gameContext.getCurrentPlayerIndex()+1) + " asked for TWOs");
+            Gdx.app.log("Action",
+                    "Player " + Integer.toString(gameContext.getCurrentPlayerIndex() + 1) + " asked for TWOs");
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.NUM_3)){
+        if (Gdx.input.isKeyPressed(Input.Keys.NUM_3)) {
             gameContext.handleTurn(new PlayerAction(gameContext.getCurrentPlayerIndex(), "ask", "THREE", "HEARTS"));
-            Gdx.app.log("Action", "Player " + Integer.toString(gameContext.getCurrentPlayerIndex()+111) + " asked for THREEs");
+            Gdx.app.log("Action",
+                    "Player " + Integer.toString(gameContext.getCurrentPlayerIndex() + 111) + " asked for THREEs");
         }
-
-
 
         // LOG HANDS FOR DEBUGGING
-        if (Gdx.input.isKeyPressed(Input.Keys.PAGE_UP)){
+        if (Gdx.input.isKeyPressed(Input.Keys.PAGE_UP)) {
             Gdx.app.log("Player1Hand", "---------Player-1-----------");
             Gdx.app.log("Player1Hand", "Hand size: " + Integer.toString(player1.get_hand().size()));
-            for (Card card : player1.get_hand()){
+            for (Card card : player1.get_hand()) {
                 Gdx.app.log("Player1Hand", card.getRank() + " of " + card.getSuit());
             }
             Gdx.app.log("Player1Hand", "----------------------------");
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.PAGE_DOWN)){
+        if (Gdx.input.isKeyPressed(Input.Keys.PAGE_DOWN)) {
             Gdx.app.log("Player2Hand", "---------Player-2-----------");
             Gdx.app.log("Player2Hand", "Hand size: " + Integer.toString(player2.get_hand().size()));
-            for (Card card : player2.get_hand()){
+            for (Card card : player2.get_hand()) {
                 Gdx.app.log("Player2Hand", card.getRank() + " of " + card.getSuit());
             }
             Gdx.app.log("Player1Hand", "----------------------------");
@@ -107,7 +111,7 @@ public class Main extends ApplicationAdapter {
         mainView.Update();
 
         // Select card on mouse click
-        if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+        if(Gdx.input.justTouched()){
             GoFish currentView = (GoFish) mainView.currentView;
             
             currentView.SelectCard();
