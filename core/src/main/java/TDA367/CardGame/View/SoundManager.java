faec2e9cba9163387d468adfc0857ef9f8e5903c
elliotn1337 <elliotn@student.chalmers.se>
2025-12-04 14:10:03 +0100

    BG_Music

--- core/src/main/java/TDA367/CardGame/View/SoundManager.java
+++ core/src/main/java/TDA367/CardGame/View/SoundManager.java
@@ -1,16 +1,20 @@
 package TDA367.CardGame.View;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 
 public class SoundManager {
 
     private static Sound hoverSound;
     private static Sound selectSound;
+    private static Music backgroundMusic;
 
     public static void load(){
         hoverSound = Gdx.audio.newSound(Gdx.files.internal("sounds/hover.wav"));
         selectSound = Gdx.audio.newSound(Gdx.files.internal("sounds/selecting_card.wav"));
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("sounds/bg_music.mp3"));
+
     }
 
     public static void playHover(){
@@ -20,6 +24,12 @@ public class SoundManager {
     public static void playSelect(){
         selectSound.play();
     }
+
+    public static void playBGMusic(){
+        backgroundMusic.setLooping(true);
+        backgroundMusic.play();
+    }
+
     public static void dispose(){
         hoverSound.dispose();
         selectSound.dispose();
