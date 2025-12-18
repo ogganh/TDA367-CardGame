27cb54d4dee25ddb793e1ebac8ac2829ead0f8dd
ASmaller <linus@ordal.se>
2025-12-18 14:29:53 +0100

    Flyttade comparison till GoFishUserPlayer

--- core/src/main/java/TDA367/CardGame/model/card_logic/Rank.java
+++ core/src/main/java/TDA367/CardGame/model/card_logic/Rank.java
@@ -14,37 +14,4 @@ public enum Rank {
     QUEEN,
     KING,
     ACE;
-
-    public static int rankToValue(String rank) {
-        switch (rank) {
-            case "TWO":
-                return 2;
-            case "THREE":
-                return 3;
-            case "FOUR":
-                return 4;
-            case "FIVE":
-                return 5;
-            case "SIX":
-                return 6;
-            case "SEVEN":
-                return 7;
-            case "EIGHT":
-                return 8;
-            case "NINE":
-                return 9;
-            case "TEN":
-                return 10;
-            case "JACK":
-                return 11;
-            case "QUEEN":
-                return 12;
-            case "KING":
-                return 13;
-            case "ACE":
-                return 14;
-            default:
-                throw new IllegalArgumentException("Unknown rank: " + rank);
-        }
-    }
 }
--- core/src/main/java/TDA367/CardGame/model/player/GoFishUserPlayer.java
+++ core/src/main/java/TDA367/CardGame/model/player/GoFishUserPlayer.java
@@ -81,9 +81,42 @@ public class GoFishUserPlayer extends UserPlayer {
     public void sortHand() {
         this.hand.sort((card1, card2) -> {
             int rankComparison = Integer.compare(
-                    Rank.rankToValue(card1.getRank()),
-                    Rank.rankToValue(card2.getRank()));
+                    rankToValue(card1.getRank()),
+                    rankToValue(card2.getRank()));
             return rankComparison;
         });
+    }
+
+    public static int rankToValue(String rank) {
+        switch (rank) {
+            case "TWO":
+                return 2;
+            case "THREE":
+                return 3;
+            case "FOUR":
+                return 4;
+            case "FIVE":
+                return 5;
+            case "SIX":
+                return 6;
+            case "SEVEN":
+                return 7;
+            case "EIGHT":
+                return 8;
+            case "NINE":
+                return 9;
+            case "TEN":
+                return 10;
+            case "JACK":
+                return 11;
+            case "QUEEN":
+                return 12;
+            case "KING":
+                return 13;
+            case "ACE":
+                return 14;
+            default:
+                throw new IllegalArgumentException("Unknown rank: " + rank);
+        }
     }
 }
